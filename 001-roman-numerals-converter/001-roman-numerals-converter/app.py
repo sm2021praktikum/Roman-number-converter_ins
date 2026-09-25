ef10545e5faac782bfb60bc8e6140ec7a33abd85
Suma Sumathi <90289323+sm2021praktikum@users.noreply.github.com>
2026-09-25 11:33:56 +0200

    Change developer name from 'Guile' to 'Sumathi'

--- 001-roman-numerals-converter/001-roman-numerals-converter/app.py
+++ 001-roman-numerals-converter/001-roman-numerals-converter/app.py
@@ -21,7 +21,7 @@ def main_post():
         number = int(alpha)
         if not 0 < number < 4000:
             return render_template('index.html', developer_name='Sumathi', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Guile')
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Sumathi')
     else:
         return render_template('index.html', developer_name='Sumathi', not_valid=False)
 
